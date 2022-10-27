ac3abd654583b02de66b7b28928325fabf364247
Jason Brammer <jbrammer@famgenix.com>
2022-10-27 17:37:59 -0400

    Jason Commit

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, request, url_for, redirect, render_template
+
 app = Flask(__name__, template_folder='template')
+
 @app.route('/')
-def home():
+def site():
 	return render_template('site.html')
+
+@app.route('/homePage', methods=['GET', 'POST'])
+def homePage():
+	if request.method == 'POST':
+        # do stuff when the form is submitted
+
+        # redirect to end the POST handling
+        # the redirect can be to the same route or somewhere else
+		return redirect(url_for('site'))
+
+    # show the form, it wasn't submitted
+	return render_template('homePage.html')
+
+@app.route('/instructions', methods=['GET', 'POST'])
+def instructions():
+	if request.method == 'POST':
+        # do stuff when the form is submitted
+
+        # redirect to end the POST handling
+        # the redirect can be to the same route or somewhere else
+		return redirect(url_for('site'))
+
+    # show the form, it wasn't submitted
+	return render_template('instructions.html')
+
 if __name__ == '__main__':
     app.run()
