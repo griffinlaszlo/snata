1efa1832dde20de65a328cc9fe79f798f53dbf6b
Noah Ho-Tai <noah@dhcp-10-7-107-67.dhcp.nd.edu>
2022-12-01 12:55:42 -0500

    second attempt

--- programs/username.py
+++ programs/username.py
@@ -5,22 +5,33 @@ import os
 import sys
 import re
 import json
-
+import datetime
 # Constants
 
-FILE = open('json/friends.json')
-file = json.load(FILE)
 
 def main():
-    print_username()
     
+    print_username()
+    first_firend()
 
+    
 
 def print_username():
 
-    FILE = open('json/account.json')
+    FILE = open('../mydata_1669823359244/json/account.json')
     file = json.load(FILE)
+
+    # FILE = open('json/account.json')
+    # file = json.load(FILE)
     print(file["Basic Information"]["Username"])
+
+def first_memory:
+
+    FILE = open('../mydata_1669823359244/json/memories_history.json')
+    file = json.load(FILE)
+
+    print(file[][])
+
 
 
 # def saved_sna(file):
@@ -33,23 +44,25 @@ def first_firend():
     file = json.load(FILE)
     print(file["Friends"][0]["Creation Timestamp"])
     the_min = [int(file["Friends"][0]["Creation Timestamp"][0:4]), int(file["Friends"][0]["Creation Timestamp"][5:7]), int(file["Friends"][0]["Creation Timestamp"][8:10]),  int(file["Friends"][0]["Creation Timestamp"][11:13]),  int(file["Friends"][0]["Creation Timestamp"][14:16]), int(file["Friends"][0]["Creation Timestamp"][17:19])]
-    print(the_min)
+    the_min_username = file["Friends"][0]["Username"]
+    #print(the_min)
     for i in file["Friends"]:
         count = 0
         current = [int(i["Creation Timestamp"][0:4]), int(i["Creation Timestamp"][5:7]), int(i["Creation Timestamp"][8:10]), int(i["Creation Timestamp"][11:13]),  int(i["Creation Timestamp"][14:16]), int(i["Creation Timestamp"][17:19])]
-        print(current)
-        whiole
-        # for :
-        #     count += 1
-        # print(i["Creation Timestamp"])
+        #print(current)
         
-    #     if int() == :
+        for k, val in enumerate(current):
+            if current[k] <= the_min[k]:
+                count += 1
+                continue
+            else:
+                break
+        if count == 6:
+            the_min = current
+            the_min_username = i["Username"]
     
-
-
-
-
-
+    print(the_min_username)
 
 if __name__ == '__main__':
     main()
+
