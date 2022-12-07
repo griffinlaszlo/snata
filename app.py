8ae57e82b2bcfb29a0cbc873e16398f9f6c1f0a4
noahhotai <noah@dhcp-10-7-107-67.dhcp.nd.edu>
2022-12-06 23:24:45 -0500

    integrated queries

--- app.py
+++ app.py
@@ -35,6 +35,21 @@ class Users(db.Model):
 	# most_sent = db.Column(db.Text())
 	media_types = db.Column(db.Text())
 	top10_text = db.Column(db.Text())
+
+	breakdown = db.Column(db.Text())
+    engagement = db.Column(db.Text())
+    num_of_interests = db.Column(db.Integer())
+
+	name_changes =  db.Column(db.Text())
+
+	link_to_memory = db.Column(db.Text())
+	first_memory_string =  db.Column(db.Text())
+
+	total_subs = db.Column(db.Integer())
+	stories = db.Column(db.Integer())
+	publishers = db.Column(db.Integer())
+	public_users = db.Column(db.Integer())
+
 	
 	data = db.Column(db.LargeBinary)
 
@@ -55,6 +70,7 @@ class Friends(db.Model):
 	username = db.Column(db.String(20), unique=True, nullable=False)
 	filename = db.Column(db.String(50))
 	data = db.Column(db.PickleType)
+	your_first_friend = db.Column(db.Text())
 
 	def __repr__(self):
 		return f'ID : {self.id}, Name : {self.username}'
@@ -64,6 +80,15 @@ class ChatHistory(db.Model):
 	username = db.Column(db.String(20), unique=True, nullable=False)
 	filename = db.Column(db.String(50))
 	data = db.Column(db.PickleType)
+
+
+	total_snaps_sent = db.Column(db.Integer())
+	total_snaps_received = db.Column(db.Integer())
+	total_snaps_saved = db.Column(db.Integer())
+	text_snaps = db.Column(db.Integer())
+	media_snaps = db.Column(db.Integer())
+	note_snaps = db.Column(db.Integer())
+	share_snaps = db.Column(db.Integer())
 
 	def __repr__(self):
 		return f'ID : {self.id}, Name : {self.username}'
@@ -106,6 +131,48 @@ def site():
 									second += ' AM'
 								month = months[int(month) - 1]
 								ct = month + " " + day + ", " + year + " at exactly " + hour + ":" + minute + ":" + second
+
+
+				def user_profile_info(file):
+
+                    breakdown = "Breakdown of Time Spent on App\n"
+                    for i in file["Breakdown of Time Spent on App"]:
+                        breakdown += i + "\n"
+
+                    engagement = "Engagement\n"
+                    for i in file["Engagement"]:
+                        engagement += f"{i['Event']}, {i['Occurences']}\n"
+
+                    num_of_interest_categories = len(file["Interest Categories"])
+
+                    return breakdown, engagement, num_of_interest_categories
+
+                breakdown, engagement, num_of_interest_categories = user_profile_info(user_profile)
+
+			with open('uploads/json/memories_profile.json', encoding="utf8") as memories_json:
+				def first_memory(file):
+
+					FILE = open('../mydata_1669823359244/json/memories_history.json')
+					file = json.load(FILE)
+					link_to_memory = file["Saved Media"][-1]["Download Link"]
+
+					return link_to_memory, f"Your first memory was taken at {file['Saved Media'][-1]['Date']}, it was a {file['Saved Media'][-1]['Media Type']}"
+					
+				link_to_memory, first_memory_string = first_memory(memories_json)
+
+			with open('uploads/json/subscriptions.json', encoding="utf8") as subscriptions_json:
+				def num_of_subscriptions(file):
+
+					public_users = len(file["Public Users"])
+					publishers = len(file["Public Users"])
+					stories = len(file["Stories"])
+					total_subs = stories + public_users + publishers
+
+					return total_subs, stories, publishers, public_users
+				
+				total_subs, stories, publishers, public_users = num_of_subscriptions(subscriptions_json)
+
+			
 			# latest and frequent locations
 			with open('uploads/json/location_history.json', encoding="utf8") as loc_json:
 				loc_history = json.load(loc_json)
@@ -136,7 +203,9 @@ def site():
 							if (i % 3 == 0):
 								freq_loc_string = freq_loc_string + loc + " ; "
 							i+=1
-						
+				
+				
+				
 			# 3 most recent received snaps
 			# do 3 people you snap the most next
 			iter = 0
@@ -213,23 +282,135 @@ def site():
 							if count == 10:
 								break
 			
-			user = Users(username=db_username, filename=file.filename, creation_time=ct, recent_location=recent_location, frequent_locations=freq_loc_string, recent_snap=recent_snap_string, top3_snappers=top3_string, most_received=most_received, media_types=media_types, top10_text=top10_text, data=file.read())
+			user = Users(username=db_username, snap_username=snap_username, snap_email=snap_email, snap_phone=snap_phone, filename=file.filename, creation_time=ct, 
+                recent_location=recent_location, frequent_locations=freq_loc_string, recent_snap=recent_snap_string, top3_snappers=top3_string, most_received=most_received, 
+                media_types=media_types, top10_text=top10_text, story_string=story_string, engagement=engagement, num_of_interests=num_of_interests, breakdown=breakdown, 
+				 name_changes = name_changes, first_memory_string = first_memory_string, link_to_memory = link_to_memory, total_subs = total_subs, stories = stories, publishers = publishers, public_users = public_users)
+
+
 
 			with open('uploads/json/location_history.json', encoding="utf8") as json_file:
 				loc = json.load(json_file)
+
+
 			location = Location(username=db_username, filename="location_history.json", data=loc)
 
 			with open('uploads/json/friends.json', encoding="utf8") as json_file:
 				frien = json.load(json_file)
-			friends = Friends(username=db_username, filename='friends.json', data=frien)
+
+
+				def first_friend(file):
+
+			
+			
+					the_min = [int(file["Friends"][0]["Creation Timestamp"][0:4]), int(file["Friends"][0]["Creation Timestamp"][5:7]), int(file["Friends"][0]["Creation Timestamp"][8:10]),  int(file["Friends"][0]["Creation Timestamp"][11:13]),  int(file["Friends"][0]["Creation Timestamp"][14:16]), int(file["Friends"][0]["Creation Timestamp"][17:19])]
+					the_min_username = file["Friends"][0]["Username"]
+					#print(the_min)
+					for i in file["Friends"]:
+						count = 0
+						current = [int(i["Creation Timestamp"][0:4]), int(i["Creation Timestamp"][5:7]), int(i["Creation Timestamp"][8:10]), int(i["Creation Timestamp"][11:13]),  int(i["Creation Timestamp"][14:16]), int(i["Creation Timestamp"][17:19])]
+						#print(current)
+						
+						for k, val in enumerate(current):
+							if current[k] <= the_min[k]:
+								count += 1
+								continue
+							else:
+								break
+						if count == 6:
+							the_min = current
+							the_min_username = i["Username"]
+							the_min_time = i["Creation Timestamp"]
+					
+					return f"{the_min_username} was your friend. You added them back on {the_min_time}"
+
+				your_first_friend = first_friend(frien)
+
+
+
+
+
+			friends = Friends(username=db_username, filename='friends.json', data=frien, your_first_friend = your_first_friend)
 
 			with open('uploads/json/chat_history.json', encoding="utf8") as json_file:
 				chat = json.load(json_file)
-			chat_history = ChatHistory(username=db_username, filename='chat_history.json', data=chat)
+				def total_snaps(file):
+
+					snaps_received_saved = len(file["Received Saved Chat History"])
+					snaps_received_unsaved = len(file["Received Unsaved Chat History"])
+					total_snaps_received = snaps_received_unsaved + snaps_received_saved
+					snaps_sent_saved = len(file["Sent Saved Chat History"])
+					snaps_sent_unsaved = len(file["Sent Unsaved Chat History"])
+					total_snaps_sent = snaps_received_unsaved + snaps_received_saved
+					total_snaps_saved = snaps_received_saved + snaps_sent_saved
+					text_snaps = 0
+					media_snaps = 0
+					share_snaps = 0
+					note_snaps = 0
+
+
+					for i in file["Received Saved Chat History"]:
+						if i["Media Type"] == "TEXT":
+							text_snaps += 1
+						elif i["Media Type"] == "MEDIA":
+							note_snaps += 1
+						elif i["Media Type"] == "NOTE":
+							note_snaps += 1
+						elif i["Media Type"] == "SHARE":
+							share_snaps += 1
+
+
+					for i in file["Received Unsaved Chat History"]:
+						if i["Media Type"] == "TEXT":
+							text_snaps += 1
+						elif i["Media Type"] == "MEDIA":
+							note_snaps += 1
+						elif i["Media Type"] == "NOTE":
+							note_snaps += 1
+						elif i["Media Type"] == "SHARE":
+							share_snaps += 1
+
+					for i in file["Sent Unsaved Chat History"]:
+						if i["Media Type"] == "TEXT":
+							text_snaps += 1
+						elif i["Media Type"] == "MEDIA":
+							note_snaps += 1
+						elif i["Media Type"] == "NOTE":
+							note_snaps += 1
+						elif i["Media Type"] == "SHARE":
+							share_snaps += 1
+
+					for i in file["Sent Saved Chat History"]:
+						if i["Media Type"] == "TEXT":
+							text_snaps += 1
+						elif i["Media Type"] == "MEDIA":
+							note_snaps += 1
+						elif i["Media Type"] == "NOTE":
+							note_snaps += 1
+						elif i["Media Type"] == "SHARE":
+							share_snaps += 1
+
+					return total_snaps_sent, total_snaps_received, total_snaps_saved, text_snaps, media_snaps, note_snaps, share_snaps
+
+				total_snaps_sent, total_snaps_received, total_snaps_saved, text_snaps, media_snaps, note_snaps, share_snaps = total_snaps(chat)
+
+			chat_history = ChatHistory(username=db_username, filename='chat_history.json', data=chat, total_snaps_sent = total_snaps_sent, total_snaps_received = total_snaps_received,
+							 total_snaps_saved = total_snaps_saved, text_snaps = text_snaps, media_snaps = media_snaps, note_snaps = note_snaps, share_snaps=share_snaps,)
 
 			with open('uploads/json/account.json', encoding="utf8") as json_file:
 				acct = json.load(json_file)
 			account = Account(username=db_username, filename='account', data=acct)
+			
+			with open('uploads/json/account_history.json', encoding="utf8") as json_file:
+				acct_history = json.load(json_file)
+				def display_name_changes(file):
+
+					name_changes = "You had a name change on:"
+					for i in file["Display Name Change"]:
+						name_changes += f"{[i['Date']}, {i['Display Name']}"
+
+    				return name_changes
+				name_changes  = display_name_changes(acct_history)
 
 			db.session.add(user)
 			db.session.add(location)
---
+++ noah_func.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# import libraries
+import os
+import sys
+import re
+import json
+
+# Constants
+
+
+def main():
+
+    FILE = open('../mydata_1669823359244/json/friends.json')
+    file = json.load(FILE)
+
+def(username):
+    print("here")
+
+
+if __name__ == "__main__":
+    main()
+
