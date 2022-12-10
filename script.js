54bce38143dc90a93e21ff4373424fd2c1ae2faf
Jason Brammer <jbrammer@famgenix.com>
2022-12-10 14:38:40 -0500

    remove loading

--- script.js
+++
@@ -1,8 +0,0 @@
-const progressBar = document.getElemmentByClassName
-('progress-bar')[0]
-setIntervals(() => {
-    const computedStyle = getComputedStyle(progressBar)
-    const width = parseFloat(computedStyle.getPropertyValue)
-    ('--width')) || 0 
-        progressBar.style.setProperty('--width', width + 0.1)
-    }, 5)
